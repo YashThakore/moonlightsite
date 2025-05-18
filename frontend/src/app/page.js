94d7e9ca4dd0c81163d84160a6168aea715153f7
YashThakore <yashthakore50@yahoo.com>
2025-05-18 17:57:22 -0400

    moonlight site

---
+++ frontend/src/app/page.js
@@ -0,0 +1,21 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function PricingPage() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://js.stripe.com/v3/pricing-table.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Choose a Plan</h1>
+      <stripe-pricing-table
+        pricing-table-id="prctbl_1RQF1yGbPDlgmUqoCbDfjHVu"
+        publishable-key="pk_live_51RQDtlGbPDlgmUqo1K9viTqQXApepgV2sscnKQRl4gzqzXp73geeTrTvzjrseCGUKsMhgYxclNVYNFUhxvKHONZQ00CjWWzWPX"
+      ></stripe-pricing-table>
+    </div>
+  );
+}
